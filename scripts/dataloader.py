b274fcfcef2712ccd82378e61ae85f69ff7d3546
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-02 16:27:50 +0100

    Update dataloader.py

--- scripts/dataloader.py
+++ scripts/dataloader.py
@@ -1,21 +1,32 @@
 
 import numpy as np
+from pathlib import Path
 
-def filereader(plate, type, file):
+def filereader(plate, type):
     import pandas as pd
     import os
+    from os.path import isfile, join
+    read = []
 
+
+    #for static cases
     if(type == 'Static' or type == 'static'): 
         type = 'Static'
-        filepath = f"../DATA/Plate {plate}/{type}/{filename}.txt"
 
+        platenameformat = plate.replace(" ","_")
+        dirpath = Path(__file__)
+        allfiles = [f for f in os.listdir(dirpath) if isfile(dirpath, f))]
+        for f in allfiles:
+            read.append(pd.read_csv(f))
+
+    #for dynamic cases
     elif(type == 'Dynamic' or type == 'dynamic'): 
         type = 'Dynamic'
         filepath = f"../DATA/Plate {plate}/{type}/{Angle}/{Frequency}/{filename}.txt"
 
-
+    print(read)
     
-    read = pd.read_csv(filepath)
+    
 
     return read
 
@@ -25,7 +36,7 @@ class data:
     def __init__(self,Plate):
         self.Plate = Plate
         
-        
+filereader('A','static')
     
 
 
