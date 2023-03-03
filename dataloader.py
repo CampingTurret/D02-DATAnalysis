38015ce4bd6e88b8acaefd0a437e9f951da8041e
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-03 06:08:31 +0100

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -12,18 +12,13 @@ def filereader(plate, type):
     #for static cases
     if(type == 'Static' or type == 'static'): 
         type = 'Static'
-
         platenameformat = plate.replace(" ","_")
-        dirpath = os.path.join('DATA','Files',f'Plate {plate}',f'{type}')
-        print(dirpath)
-        dirpath = dirpath.replace(r"\\",r'\a'.replace('a',''))
-        print(dirpath)
-        input()
+        dirpath = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'.','DATA',f'Plate {plate}',f'{type}'))
         allfiles = [f for f in os.listdir(dirpath)]
         print(allfiles)
-        input()
         for f in allfiles:
-            read.append(pd.read_csv(f))
+            tempf = os.path.join(dirpath,f)
+            read.append(pd.read_csv(tempf,encoding = 'cp1252'))
 
     #for dynamic cases
     elif(type == 'Dynamic' or type == 'dynamic'): 
@@ -41,8 +36,11 @@ class data:
 
     def __init__(self,Plate):
         self.Plate = Plate
+        self.static = filereader(Plate,'static')
+        self.dynamic = filereader(Plate,'Dynamic')
         
-filereader('A','static')
+Pa = data('A')
+print(Pa.static)
     
 
 
