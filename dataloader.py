65eb39dbf76b416d918e49850abca5b5235335a6
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-03 11:42:36 +0100

    it now works

--- dataloader.py
+++ dataloader.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def filereader(plate, type, Angle = None, Frequency = None):
     import pandas as pd
@@ -16,8 +16,8 @@ def filereader(plate, type, Angle = None, Frequency = None):
             tempf = os.path.join(dirpath,f)
             data = pd.read_csv(tempf,encoding = 'cp1252')
             data = data.dropna(axis = 1)
-            data.set_axis(headerrow,axis = 1,copy=False)
-            read.append(data)
+            data = data.set_axis(headerrow,axis = 1,copy=True)
+            read.append([data,f])
 
     #for dynamic cases
     elif(type == 'Dynamic' or type == 'dynamic'): 
@@ -28,36 +28,70 @@ def filereader(plate, type, Angle = None, Frequency = None):
             tempf = os.path.join(dirpath,f)
             data = pd.read_csv(tempf,encoding = 'cp1252')
             data = data.dropna(axis = 1)
-            data.set_axis(headerrow,axis = 1,copy=False)
-            read.append(data)
-    return read
+            data =data.set_axis(headerrow,axis = 1,copy=True)
+            read.append([data,f])
+
+    
+    out = np.array(read, dtype=object)        
+    return out
 
 
 class data:
 
+    #Initialise plate
     def __init__(self,Plate):
         self.Plate = Plate
         self.static = filereader(Plate,'static')
      
+    #returns the array with the all the data from 1 dynamic case and loads it into object 
     def Get_Dynamic(self,angle,frequency):
           
         A = str(angle)
         F = str(frequency).replace(".","")
 
         Dynamiccase = filereader(self.Plate,'Dynamic',A,F)
+        self.dynamicloaded = Dynamiccase
         return Dynamiccase
 
+
+    #gets the indexes of the free and locked cases
     def Split_static(self):
         
+        listfreeindex = np.empty((0,1), dtype=int)
+        listlockedindex = np.empty((0,1), dtype=int)
+        #get the stringnames with either locked or free in it
+        for datafile in self.static[:,1]:     
+            idfile = np.where(self.static[:,1] == datafile)[0][0]
+            name = datafile
+            name = name.split('_')
+        
+            for part in name:
+                if(part == "Free"):
+                    listfreeindex = np.append(listfreeindex ,idfile)
+                if(part == "Locked"):
+                    listlockedindex = np.append(listlockedindex,idfile)
 
+        self.static_free_index = listfreeindex
+        self.static_locked_index = listlockedindex
 
-        self.static_free = 0
-        self.static_locked = 0
+    #returns the array with only data from the free hinges
+    def Get_Static_Free(self):
+        self.Split_static()
+        splitdata = self.static[self.static_free_index]
+        return splitdata
 
+    #returns the array with only data from the locked hinges
+    def Get_Static_Locked(self):
+        self.Split_static()
+        splitdata = self.static[self.static_locked_index]
+        return splitdata
+    
+    
     
         
 Pa = data('A')
-print(Pa.static)
+#Pa.Split_static()
+print(Pa.Get_Static_Free())
 #print(Pa.Get_Dynamic(0,0.5))
     
 
