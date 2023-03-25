b2204d393d4647035bbd41fc192ba86875d838fd
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-25 23:29:30 +0100

    pushing models

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 Pa = data('B',5,0.5,'cuda')
 #Pa.Get_Dynamic()
-#Pa.Split_Dynamic_Loaded(0)
+#Pa.Split_Dynamic_Loaded(5)
 #Pa.Remove_Gaps_Dynamic()
 #Pa.Plot_Raw_2D_Loaded("Time [s]","Bending [N-mm]")
 #Pa.run_analysis_2D()
