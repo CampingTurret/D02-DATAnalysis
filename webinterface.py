1581493baf0515cdeed9bc5cc347ff45bdd8c85a
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-30 12:49:53 +0200

    push

--- webinterface.py
+++ webinterface.py
@@ -21,11 +21,28 @@ def index():
         Plate =request.form['input1']
         AOA = request.form['input2']
         hz = request.form['input3']
-
-
+        limit = request.form['limit']
+        print(limit)
+        
         Pa = data(Plate,AOA,hz,'cpu')
-        Pa.run_analysis_2D_Quick('quick',show=False)
+        if(limit ==1 ):
+            selected = []
+            if (request.form['case1'] == 1): selected.append('Free') 
+            if (request.form['case2'] == 1): selected.append('Locked')
+            if (request.form['case3'] == 1): selected.append('Pre')
+            if (request.form['case4'] == 1): selected.append('Rel0')
+            if (request.form['case5'] == 1): selected.append('Rel50')
+            if (request.form['case6'] == 1): selected.append('Rel100')
+
+            Pa.run_analysis_2D_Quick('limit',show=False)
+
+        if(limit !=1 ):
+            Pa.run_analysis_2D_Quick('quick',show=False)
+
         # Generate image using Matplotlib
+        plt.legend()
+        plt.xlabel('Time [s]')
+        plt.ylabel('Bending [N-mm]')
         plt.savefig(os.path.abspath(os.path.join(os.path.dirname( __file__ ),'.','static','plot.png')))
         plt.clf()
         plot_url = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'.','static','plot.png'))
