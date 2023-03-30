9a83ad4b0142a6239125dc9221639992db4ae7c4
CampingTurret <l.gaeta@student.tudelft.nl>
2023-03-30 14:12:19 +0200

    Update webinterface.py

--- webinterface.py
+++ webinterface.py
@@ -24,19 +24,19 @@ def index():
         limit = request.form.get('limit')
         print(request.form.get('limit') != None)
         Pa = data(Plate,AOA,hz,'cpu')
-        if(int(limit) ==1 ):
+        if(request.form.get('limit') != None):
            
             selected = []
             if (request.form.get('case1') != None): selected.append('Free') 
             if (request.form.get('case2') != None): selected.append('Locked')
-            if (int(request.form.get('case3')) == 1): selected.append('Pre')
-            if (int(request.form.get('case4')) == 1): selected.append('Rel0')
-            if (int(request.form.get('case5')) == 1): selected.append('Rel50')
-            if (int(request.form.get('case6')) == 1): selected.append('Rel100')
+            if (request.form.get('case3') != None): selected.append('Pre')
+            if (request.form.get('case4') != None): selected.append('Rel0')
+            if (request.form.get('case5') != None): selected.append('Rel50')
+            if (request.form.get('case6') != None): selected.append('Rel100')
 
             Pa.run_analysis_2D_Quick('limit',show=False,types = selected)
 
-        if(limit !=1 ):
+        if(request.form.get('limit') == None ):
             Pa.run_analysis_2D_Quick('quick',show=False)
 
         # Generate image using Matplotlib
